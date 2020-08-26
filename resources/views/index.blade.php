409ca92bbce58310a93e70bc9b256f3f823b8832
fimfim0503 <fimfim0503@gmail.com>
2020-08-26 10:03:08 +0700

    pertama komit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+
+    @extends('layout.main');
+
+
+     @section ('title', 'Web Programmging ')
+
+     @section ('container')
+        <div class="container">
+        <div class="row">
+            <div class="col-10">
+              <h1 class="mt-2">Hello Index</h1>
+            </div>
+          </div>
+        
+        </div>
+    @endsection
+
+    
